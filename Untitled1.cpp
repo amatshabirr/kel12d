e7141270d6db787a5adb34a7915081e99ee2e42d
amatshabirr <ahmadad3005@gmail.com>
2022-03-18 07:51:17 -0700

    membuat main.cpp

---
+++ Untitled1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int pil,bil1, bil2;
+	float hasil;
+	cout<<"Program Kalkulator"<<endl;
+	cout<<"Menu"<<endl;
+	cout<<"1. Pertambahan"<<endl;
+	cout<<"2. Pengurangan"<<endl;
+	cout<<"3. Perkalian"<<endl;
+	cout<<"4. Pembagian"<<endl;
+	cout<<"Masukkan Pilihan";cin>>pil;
+	cout<<"Bil1 = ";cin>>bil1;
+	cout<<"Bil2 = ";cin>>bil2;
+	
+	if(pil==1){
+		hasil = bil1 + bil2;
+		cout<<"bil1"<<" + "<<"bill2"<<" = "<<hasil<<endl;
+	}
+	else if(pil==2){
+		hasil = bil1 - bil2;
+		cout<<"bil1"<<" - "<<"bill2"<<" = "<<hasil<<endl;
+}
+	else if(pil==3){
+		hasil = bil1 * bil2;
+		cout<<"bil1"<<" x "<<"bill2"<<" = "<<hasil<<endl;
+}
+	else{
+		hasil = bil1 / bil2;
+		cout<<"bil1"<<" : "<<"bill2"<<" = "<<hasil<<endl;
+	}
+	
+	return 0;
+}
